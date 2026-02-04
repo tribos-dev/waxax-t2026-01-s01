49bef0200b3cb6753de2faf845642b050d4aee80
Felipe Moraski <moraski060@gmail.com>
2026-02-03 21:47:17 -0300

    feat: Implementação do DTO de Projeção e Mapper

--- src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/response/ClienteListAllResponse.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/response/ClienteListAllResponse.java
@@ -1,4 +1,31 @@
 package br.com.wakax.wakax_ecommerce.cliente.application.api.response;
 
+import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
+import br.com.wakax.wakax_ecommerce.pessoa.domain.StatusPessoa;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ClienteListAllResponse {
+
+    private String nome;
+    private String email;
+    private StatusPessoa statusPessoa;
+
+
+
+    public ClienteListAllResponse(Cliente cliente) {
+        this.nome = cliente.getPessoa().getNome();
+        this.email =
+                cliente.getPessoa().getEmails() != null
+                        ? cliente.getPessoa().getEmails().stream().findFirst().orElse(null)
+                        : null;
+        this.statusPessoa = cliente.getPessoa().getStatus();
+
+    }
+
+    public static List<ClienteListAllResponse> toList(List<Cliente> clientes) {
+        return clientes.stream().map(ClienteListAllResponse::new).collect(Collectors.toList());
+    }
+
 }
