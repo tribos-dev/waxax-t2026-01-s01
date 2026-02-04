2cefe209f63d10ed74c81d0962c6075731da4c51
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-02-03 21:53:17 -0300

    feat - Implementa camada repository com query otimizada

--- src/main/java/br/com/wakax/wakax_ecommerce/produto/application/repository/ProdutoRepository.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/produto/application/repository/ProdutoRepository.java
@@ -1,6 +1,10 @@
 package br.com.wakax.wakax_ecommerce.produto.application.repository;
 
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.wakax.wakax_ecommerce.produto.domain.Produto;
 
@@ -8,4 +12,7 @@ public interface ProdutoRepository {
   Produto salva(Produto produto);
 
   Produto buscaProdutoPorId(UUID idProduto);
+
+  
+  Page<Produto> listarTodosProdutosPaginado(Pageable pageable);
 }
--- src/main/java/br/com/wakax/wakax_ecommerce/produto/infra/ProdutoJPARepository.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/produto/infra/ProdutoJPARepository.java
@@ -1,8 +1,11 @@
 package br.com.wakax.wakax_ecommerce.produto.infra;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +15,13 @@ import br.com.wakax.wakax_ecommerce.produto.domain.Produto;
 public interface ProdutoJPARepository extends JpaRepository<Produto, UUID> {
   @Query("SELECT p FROM Produto p LEFT JOIN FETCH p.precos WHERE p.id = :id")
   Optional<Produto> findByIdComPrecos(@Param("id") UUID id);
+  
+  @Query("SELECT DISTINCT p FROM Produto p LEFT JOIN FETCH p.precos")
+  List<Produto> findAllComPrecos();
+  
+  @Query(value = "SELECT DISTINCT p FROM Produto p LEFT JOIN FETCH p.precos",
+         countQuery = "SELECT COUNT(DISTINCT p) FROM Produto p")
+  Page<Produto> findAllComPrecosPaginado(Pageable pageable);
 
   boolean existsByDescricao(String descricao);
 }
