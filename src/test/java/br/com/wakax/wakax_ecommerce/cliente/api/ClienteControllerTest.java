faa96d7d0976054b39c1b02bd8f03129b6d4efdd
Felipe Moraski <moraski060@gmail.com>
2026-02-06 00:29:47 -0300

    fix: Correção no ClienteControllerTest

--- src/test/java/br/com/wakax/wakax_ecommerce/cliente/api/ClienteControllerTest.java
+++ src/test/java/br/com/wakax/wakax_ecommerce/cliente/api/ClienteControllerTest.java
@@ -3,6 +3,7 @@ package br.com.wakax.wakax_ecommerce.cliente.api;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,8 @@ import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteList
 import br.com.wakax.wakax_ecommerce.cliente.application.api.response.PageResponse;
 import br.com.wakax.wakax_ecommerce.cliente.application.service.ClienteService;
 import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
+import br.com.wakax.wakax_ecommerce.pessoa.domain.Pessoa;
+import br.com.wakax.wakax_ecommerce.pessoa.domain.StatusPessoa;
 
 @ExtendWith(MockitoExtension.class)
 class ClienteControllerTest {
@@ -30,14 +33,21 @@ class ClienteControllerTest {
   @Test
   void deveListarClientesComSucesso() {
     PageRequest pageable = PageRequest.of(0, 10);
+    Pessoa pessoa = mock(Pessoa.class);
+    when(pessoa.getNome()).thenReturn("João");
+    when(pessoa.getEmails()).thenReturn(List.of("joao@email.com"));
+    when(pessoa.getStatus()).thenReturn(StatusPessoa.ATIVO);
     Cliente cliente = mock(Cliente.class);
+    when(cliente.getPessoa()).thenReturn(pessoa);
+    when(cliente.getDataCriacao()).thenReturn(LocalDateTime.now());
+    when(cliente.getDataEdicao()).thenReturn(LocalDateTime.now());
     Page<Cliente> page = new PageImpl<>(List.of(cliente), pageable, 1);
     when(clienteService.buscarTodosOsClientes(pageable)).thenReturn(page);
     PageResponse<ClienteListAllResponse> response = clienteController.buscarTodosOsClientes(0, 10);
     assertNotNull(response);
     assertEquals(1, response.getTotalDeUsuarios());
     assertEquals(1, response.getContent().size());
-    verify(clienteService, times(1)).buscarTodosOsClientes(pageable);
+    verify(clienteService).buscarTodosOsClientes(pageable);
   }
 
   @Test
