4533d944c5f9e7a580d3bd69a95fb7b3aef0de50
Felipe Moraski <moraski060@gmail.com>
2026-02-12 22:26:58 -0300

    feat: Implementação da camada Service

--- src/main/java/br/com/wakax/wakax_ecommerce/carrinho/application/service/CarrinhoService.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/carrinho/application/service/CarrinhoService.java
@@ -1,12 +1,16 @@
 package br.com.wakax.wakax_ecommerce.carrinho.application.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import br.com.wakax.wakax_ecommerce.carrinho.api.request.ItemCarrinhoRequest;
 import br.com.wakax.wakax_ecommerce.carrinho.api.response.CarrinhoResponse;
+import br.com.wakax.wakax_ecommerce.carrinho.api.response.CarrinhosListAllResponse;
 
 public interface CarrinhoService {
   CarrinhoResponse adicionaItemNoCarrinho(UUID idCliente, ItemCarrinhoRequest itemCarrinho);
 
   CarrinhoResponse buscaCarrinhoPorId(UUID idCliente, UUID idCarrinho);
+
+  List<CarrinhosListAllResponse> buscarTodosOsCarrinhos(UUID idCliente);
 }
