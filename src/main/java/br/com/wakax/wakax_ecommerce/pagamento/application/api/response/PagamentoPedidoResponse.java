45aa770dd53287ffa272852cd575cebe2c840cb7
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-02-13 12:54:30 -0300

    feat: cria DTO PagamentoPedidoResponse

--- src/main/java/br/com/wakax/wakax_ecommerce/pagamento/application/api/response/PagamentoPedidoResponse.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/pagamento/application/api/response/PagamentoPedidoResponse.java
@@ -2,7 +2,6 @@ package br.com.wakax.wakax_ecommerce.pagamento.application.api.response;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import br.com.wakax.wakax_ecommerce.pagamento.domain.Pagamento;
 import br.com.wakax.wakax_ecommerce.pagamento.domain.StatusPagamento;
@@ -10,20 +9,16 @@ import br.com.wakax.wakax_ecommerce.pedido.domain.FormaPagamento;
 import lombok.Getter;
 
 @Getter
-public class PagamentoResponse {
-  private final UUID idPagamento;
-  private final UUID pedidoId;
+public class PagamentoPedidoResponse {
   private final StatusPagamento statusPagamento;
-  private final LocalDateTime dataPagamento;
   private final BigDecimal valor;
+  private final LocalDateTime dataPagamento;
   private final FormaPagamento metodoPagamento;
 
-  public PagamentoResponse(Pagamento pagamento) {
-    this.idPagamento = pagamento.getId();
-    this.pedidoId = pagamento.getPedido().getId();
+  public PagamentoPedidoResponse(Pagamento pagamento) {
     this.statusPagamento = pagamento.getStatusPagamento();
-    this.dataPagamento = pagamento.getDataPagamento();
     this.valor = pagamento.getValor();
+    this.dataPagamento = pagamento.getDataPagamento();
     this.metodoPagamento = pagamento.getPedido().getFormaPagamento();
   }
 }
