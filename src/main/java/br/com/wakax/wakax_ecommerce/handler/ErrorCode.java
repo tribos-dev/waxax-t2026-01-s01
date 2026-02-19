96045e8ce8e3d0f0f78b6ec637a82caa97707292
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-02-19 06:37:53 -0300

    fix: resolve conflitos

--- src/main/java/br/com/wakax/wakax_ecommerce/handler/ErrorCode.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/handler/ErrorCode.java
@@ -24,8 +24,10 @@ public enum ErrorCode {
   QUANTIDADE_INVALIDA("quantidade.invalida"),
   CUSTO_INVALIDO("custo.invalido"),
   ESTOQUE_INVALIDO("estoque.invalido"),
-  STATUS_PAGAMENTO_INVALIDO("status.de.pagamento.invalido"),
-  PEDIDO_NAO_POSSUI_PAGAMENTO("pedido.nao.possui.pagamento");
+  PEDIDO_NAO_POSSUI_PAGAMENTO("pedido.nao.possui.pagamento"),
+  CLIENTE_NAO_E_DONO_DO_PEDIDO("cliente.nao.e.dono.do.pedido"),
+  PEDIDO_NAO_POSSUI_RASTREIO("pedido.nao.possui.rastreio"),
+  STATUS_PAGAMENTO_INVALIDO("status.de.pagamento.invalido");
 
   private final String code;
 
