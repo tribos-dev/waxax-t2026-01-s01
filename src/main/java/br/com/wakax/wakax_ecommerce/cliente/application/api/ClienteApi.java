318fc5b8443c49dde559bcb4c121c170090cf912
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-02-27 22:09:43 -0300

    fix: realiza correcao com spoteless camada api e dto ClienteAtualizaResponse

--- src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/ClienteApi.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/ClienteApi.java
@@ -4,12 +4,12 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
-import br.com.wakax.wakax_ecommerce.cliente.application.api.request.ClienteAtualizaRequest;
-import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteAtualizaResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import br.com.wakax.wakax_ecommerce.cliente.application.api.request.ClienteAtualizaRequest;
 import br.com.wakax.wakax_ecommerce.cliente.application.api.request.ClienteRequest;
+import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteAtualizaResponse;
 import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteListAllResponse;
 import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteResponse;
 import br.com.wakax.wakax_ecommerce.cliente.application.api.response.PageResponse;
@@ -30,5 +30,6 @@ public interface ClienteApi {
       @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size);
 
   @PatchMapping("/{idCliente}")
-  ClienteAtualizaResponse atualizarCliente(@PathVariable UUID idCliente, @RequestBody @Valid ClienteAtualizaRequest clienteRequest);
+  ClienteAtualizaResponse atualizarCliente(
+      @PathVariable UUID idCliente, @RequestBody @Valid ClienteAtualizaRequest clienteRequest);
 }
--- src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/response/ClienteAtualizaResponse.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/response/ClienteAtualizaResponse.java
@@ -1,5 +1,7 @@
 package br.com.wakax.wakax_ecommerce.cliente.application.api.response;
 
+import java.time.LocalDateTime;
+
 import br.com.wakax.wakax_ecommerce.cliente.application.api.request.ClienteAtualizaRequest;
 import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
 import br.com.wakax.wakax_ecommerce.pessoa.domain.Endereco;
@@ -7,31 +9,28 @@ import br.com.wakax.wakax_ecommerce.pessoa.domain.StatusPessoa;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.time.LocalDateTime;
-
 @Getter
 @ToString
 public class ClienteAtualizaResponse {
-    private String nome;
-    private String email;
-    private Endereco endereco;
-    private StatusPessoa statusPessoa;
-    private LocalDateTime dataEdicao;
+  private String nome;
+  private String email;
+  private Endereco endereco;
+  private StatusPessoa statusPessoa;
+  private LocalDateTime dataEdicao;
 
+  public ClienteAtualizaResponse(Cliente cliente, ClienteAtualizaRequest request) {
+    this.nome = cliente.getPessoa().getNome();
+    this.dataEdicao = cliente.getDataEdicao();
+    this.statusPessoa = cliente.getPessoa().getStatus();
 
-    public ClienteAtualizaResponse(Cliente cliente, ClienteAtualizaRequest request) {
-        this.nome = cliente.getPessoa().getNome();
-        this.dataEdicao = cliente.getDataEdicao();
-        this.statusPessoa = cliente.getPessoa().getStatus();
+    if (request.getEmails() != null && !request.getEmails().isEmpty()) {
 
-        if (request.getEmails() != null && !request.getEmails().isEmpty()) {
-
-            this.email = request.getEmails().get(request.getEmails().size() - 1);
-        }
-
-        if (request.getEnderecos() != null && !request.getEnderecos().isEmpty()) {
-
-            this.endereco = request.getEnderecos().get(request.getEnderecos().size() - 1);
-        }
+      this.email = request.getEmails().get(request.getEmails().size() - 1);
     }
+
+    if (request.getEnderecos() != null && !request.getEnderecos().isEmpty()) {
+
+      this.endereco = request.getEnderecos().get(request.getEnderecos().size() - 1);
+    }
+  }
 }
