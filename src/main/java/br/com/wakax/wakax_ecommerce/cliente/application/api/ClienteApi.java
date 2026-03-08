d75aca4dc42b658db1ac7bbda1db2a781e74ccc4
Ariane Pinheiro <arianepinheirodev@gmail.com>
2026-03-08 17:22:15 -0300

    feat:implementa contrato API

--- src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/ClienteApi.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/ClienteApi.java
@@ -26,4 +26,8 @@ public interface ClienteApi {
   @GetMapping("/clientes")
   PageResponse<ClienteListAllResponse> buscarTodosOsClientes(
       @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size);
+
+  @PatchMapping("/{idCliente}/ativar")
+  @ResponseStatus(HttpStatus.OK)
+  ClienteResponse ativarCliente(@PathVariable UUID idCliente);
 }
