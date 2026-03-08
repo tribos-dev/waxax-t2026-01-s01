953f1f1da26237be8813e22309162122b01ae87a
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-03-08 10:21:52 -0300

    fix: Efetuado correção com spotless

--- src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/response/ClienteAtualizaResponse.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/response/ClienteAtualizaResponse.java
@@ -2,7 +2,6 @@ package br.com.wakax.wakax_ecommerce.cliente.application.api.response;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
 import br.com.wakax.wakax_ecommerce.pessoa.domain.StatusPessoa;
--- src/main/java/br/com/wakax/wakax_ecommerce/pessoa/domain/Endereco.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/pessoa/domain/Endereco.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
