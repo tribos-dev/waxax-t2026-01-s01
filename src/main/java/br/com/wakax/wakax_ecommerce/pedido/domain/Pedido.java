49a21186409412baf5f432347a97b37ed0a6c4f6
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-03-08 13:51:51 -0300

    feat: implementa regra de negocio

--- src/main/java/br/com/wakax/wakax_ecommerce/pedido/domain/Pedido.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/pedido/domain/Pedido.java
@@ -9,9 +9,13 @@ import java.util.stream.Collectors;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 
+import org.springframework.http.HttpStatus;
+
 import br.com.wakax.wakax_ecommerce.carrinho.domain.Carrinho;
 import br.com.wakax.wakax_ecommerce.carrinho.domain.ItemCarrinho;
 import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
+import br.com.wakax.wakax_ecommerce.handler.APIException;
+import br.com.wakax.wakax_ecommerce.handler.ErrorCode;
 import br.com.wakax.wakax_ecommerce.pedido.application.api.request.PedidoRequest;
 import br.com.wakax.wakax_ecommerce.pessoa.domain.Endereco;
 import lombok.*;
@@ -60,9 +64,14 @@ public class Pedido {
   @OneToOne(mappedBy = "pedido")
   private Rastreamento rastreamento;
 
+  @Column(nullable = false)
+  @NotNull
+  private LocalDateTime dataUltimaAtualizacao;
+
   public Pedido(PedidoRequest request, Carrinho carrinho) {
     this.cliente = carrinho.getCliente();
     this.dataPedido = LocalDateTime.now();
+    this.dataUltimaAtualizacao = LocalDateTime.now();
     this.status = StatusPedido.CRIADO;
     this.formaPagamento = request.getFormaPagamento();
     this.enderecoEntrega = carrinho.getCliente().getPessoa().getEnderecos().get(0);
@@ -97,5 +106,26 @@ public class Pedido {
 
   public void aguardarPagamento() {
     this.status = StatusPedido.AGUARDANDO_PAGAMENTO;
+  }
+
+  public void atualizarStatus(StatusPedido novoStatus) {
+    if (!podeMudarPara(novoStatus)) {
+      throw new APIException(
+          HttpStatus.BAD_REQUEST, ErrorCode.TRANSICAO_STATUS_INVALIDA, this.status, novoStatus);
+    }
+    this.status = novoStatus;
+    this.dataUltimaAtualizacao = LocalDateTime.now();
+  }
+
+  private boolean podeMudarPara(StatusPedido novoStatus) {
+    return switch (this.status) {
+      case CRIADO -> novoStatus == StatusPedido.AGUARDANDO_PAGAMENTO
+          || novoStatus == StatusPedido.CANCELADO;
+      case AGUARDANDO_PAGAMENTO -> novoStatus == StatusPedido.PAGO
+          || novoStatus == StatusPedido.CANCELADO;
+      case PAGO -> novoStatus == StatusPedido.ENVIADO;
+      case ENVIADO -> novoStatus == StatusPedido.ENTREGUE;
+      case ENTREGUE, CANCELADO -> false;
+    };
   }
 }
