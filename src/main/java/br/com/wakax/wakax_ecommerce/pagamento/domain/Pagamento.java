2e1039b9061e85247d7b82820e5c3becc4fb246b
Felipe Moraski <moraski060@gmail.com>
2026-03-10 00:05:09 -0300

    feat: implementa métodos para reprocessamento no dominio de Pagamento

--- src/main/java/br/com/wakax/wakax_ecommerce/pagamento/domain/Pagamento.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/pagamento/domain/Pagamento.java
@@ -7,8 +7,11 @@ import java.util.UUID;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 
+import br.com.wakax.wakax_ecommerce.handler.APIException;
+import br.com.wakax.wakax_ecommerce.handler.ErrorCode;
 import br.com.wakax.wakax_ecommerce.pedido.domain.Pedido;
 import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Entity
 @Data
@@ -37,6 +40,13 @@ public class Pagamento {
   @PositiveOrZero
   private BigDecimal valor;
 
+  @Column(nullable = false)
+  @Builder.Default
+  @NotNull
+  private int tentativasPagamento = 0;
+
+  private static final int MAX_TENTATIVAS = 3;
+
   public Pagamento(Pedido pedido) {
     this.pedido = pedido;
     this.statusPagamento = StatusPagamento.AGUARDANDO;
@@ -50,5 +60,30 @@ public class Pagamento {
 
   public void aguardarPagamento() {
     this.statusPagamento = StatusPagamento.AGUARDANDO;
+  }
+
+  public void prepararReprocessamento() {
+
+    if (this.statusPagamento == StatusPagamento.PAGO) {
+      throw new APIException(
+              HttpStatus.CONFLICT,
+              ErrorCode.PAGAMENTO_JA_PROCESSADO_COM_SUCESSO,
+              this.getStatusPagamento());
+    }
+    if (this.statusPagamento != StatusPagamento.FALHOU) {
+      throw new APIException(
+              HttpStatus.CONFLICT,
+              ErrorCode.PAGAMENTO_NAO_PODE_SER_REPROCESSADO,
+              this.getStatusPagamento());
+    }
+    if (this.tentativasPagamento >= MAX_TENTATIVAS) {
+      throw new APIException(
+              HttpStatus.CONFLICT,
+              ErrorCode.LIMITE_DE_TENTATIVAS_EXCEDIDO,
+              this.getStatusPagamento());
+    }
+    this.tentativasPagamento++;
+    this.statusPagamento = StatusPagamento.AGUARDANDO;
+    this.dataPagamento = LocalDateTime.now();
   }
 }
