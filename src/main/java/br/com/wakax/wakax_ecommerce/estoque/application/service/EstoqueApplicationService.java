5041405b42d14568770ac733913d3e8915b6e90d
iago <iagopsilva073@gmail.com>
2026-03-10 19:11:20 -0300

    fix: reinclusão de anotacoes

--- src/main/java/br/com/wakax/wakax_ecommerce/estoque/application/service/EstoqueApplicationService.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/estoque/application/service/EstoqueApplicationService.java
@@ -65,6 +65,8 @@ public class EstoqueApplicationService implements EstoqueService {
     return disponivel;
   }
 
+    @Override
+    @Transactional
   public void reservaQuantidade(UUID idProduto, Integer quantidade) {
     log.info("[start] EstoqueApplicationService - reservaQuantidade");
     Estoque estoque = buscaEstoqueExistente(idProduto);
