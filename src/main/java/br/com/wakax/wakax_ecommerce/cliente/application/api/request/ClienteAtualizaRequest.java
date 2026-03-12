c073a792da7a13277a140baad4b95cb4e61ccb4f
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-03-12 13:28:30 -0300

    fix: atribui/remove anotações necessarias

--- src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/request/ClienteAtualizaRequest.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/request/ClienteAtualizaRequest.java
@@ -3,19 +3,20 @@ package br.com.wakax.wakax_ecommerce.cliente.application.api.request;
 import java.util.List;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import br.com.wakax.wakax_ecommerce.pessoa.domain.Endereco;
 import lombok.Getter;
 
 @Getter
 public class ClienteAtualizaRequest {
+  @NotBlank
   @Size(max = 150, message = "Nome deve ter no máximo 150 caracteres")
   private String nome;
 
-  private List<@Email @Size(max = 150) String> emails;
+  private List<@Email @NotBlank(message = "Email inválido") @Size(max = 150) String> emails;
 
-  private List<@Size(max = 20) String> telefones;
+  private List<@NotBlank @Size(max = 15) String> telefones;
 
-  private List<Endereco> enderecos;
+  private List<ClienteEnderecoRequest> enderecos;
 }
