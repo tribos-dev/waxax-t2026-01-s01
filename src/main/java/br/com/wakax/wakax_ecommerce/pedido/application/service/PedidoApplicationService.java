1d2584c119945ce0831a7941d9e55ca7e99e1b15
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-03-16 23:13:47 -0300

    fix: remove metodo if, e implementa metodo validaSeClienteEstaAtivo

--- src/main/java/br/com/wakax/wakax_ecommerce/pedido/application/service/PedidoApplicationService.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/pedido/application/service/PedidoApplicationService.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import br.com.wakax.wakax_ecommerce.carrinho.application.repository.CarrinhoRepository;
@@ -17,7 +16,6 @@ import br.com.wakax.wakax_ecommerce.carrinho.domain.Carrinho;
 import br.com.wakax.wakax_ecommerce.cliente.application.service.ClienteService;
 import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
 import br.com.wakax.wakax_ecommerce.estoque.application.service.EstoqueService;
-import br.com.wakax.wakax_ecommerce.handler.APIException;
 import br.com.wakax.wakax_ecommerce.pedido.application.api.PedidoListResponse;
 import br.com.wakax.wakax_ecommerce.pedido.application.api.PedidoPageResponse;
 import br.com.wakax.wakax_ecommerce.pedido.application.api.request.PedidoRequest;
@@ -26,7 +24,6 @@ import br.com.wakax.wakax_ecommerce.pedido.application.api.response.PedidoRespon
 import br.com.wakax.wakax_ecommerce.pedido.application.repository.PedidoRepository;
 import br.com.wakax.wakax_ecommerce.pedido.domain.Pedido;
 import br.com.wakax.wakax_ecommerce.pedido.domain.StatusPedido;
-import br.com.wakax.wakax_ecommerce.pessoa.domain.StatusPessoa;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -46,10 +43,7 @@ public class PedidoApplicationService implements PedidoService {
     log.info("[start] PedidoApplicationService - cadastraPedido");
     Carrinho carrinho = carrinhoRepository.buscaCarrinhoPorId(request.getIdCarrinho());
     Cliente cliente = carrinho.getCliente();
-    if (cliente.getPessoa().getStatus() == StatusPessoa.INATIVO) {
-      throw APIException.build(
-          HttpStatus.CONFLICT, "Cliente está inativo e não pode realizar pedidos");
-    }
+    cliente.validaSeClienteEstaAtivo();
     Pedido pedido = new Pedido(request, carrinho);
     pedidoRepository.salva(pedido);
     carrinho.finalizar();
