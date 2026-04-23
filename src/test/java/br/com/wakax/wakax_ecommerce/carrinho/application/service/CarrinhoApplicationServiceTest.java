9db91d994e3da26065c8f7d33d4a5472f3911fc5
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-04-23 20:22:42 -0300

    fix:cefetua coreções em testes unitarios

--- src/test/java/br/com/wakax/wakax_ecommerce/carrinho/application/service/CarrinhoApplicationServiceTest.java
+++ src/test/java/br/com/wakax/wakax_ecommerce/carrinho/application/service/CarrinhoApplicationServiceTest.java
@@ -210,14 +210,9 @@ class CarrinhoApplicationServiceTest {
 
     when(carrinhoRepository.buscaCarrinhoPorId(carrinho.getId())).thenReturn(carrinho);
 
-    when(carrinhoRepository.carrinhoPertenceAoUsuario(carrinho.getId(), email)).thenReturn(true);
-
     applicationService.deletaItemDoCarrinho(email, carrinho.getId(), idItem);
 
     verify(carrinhoRepository, times(1)).buscaCarrinhoPorId(carrinho.getId());
-
-    verify(carrinhoRepository, times(1)).carrinhoPertenceAoUsuario(carrinho.getId(), email);
-
     verify(carrinhoRepository, times(1)).salva(carrinho);
   }
 
@@ -247,8 +242,6 @@ class CarrinhoApplicationServiceTest {
     String email = "outro@email.com";
 
     when(carrinhoRepository.buscaCarrinhoPorId(carrinho.getId())).thenReturn(carrinho);
-
-    when(carrinhoRepository.carrinhoPertenceAoUsuario(carrinho.getId(), email)).thenReturn(false);
 
     assertThrows(
         APIException.class,
