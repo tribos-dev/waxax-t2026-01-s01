aef67f65689fdc4038a2a0ec1e7fa227e945ce93
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2026-05-10 19:46:41 -0300

    feat: implementa metodos e tabelas em domain preco

--- src/main/java/br/com/wakax/wakax_ecommerce/produto/domain/Preco.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/produto/domain/Preco.java
@@ -1,6 +1,7 @@
 package br.com.wakax.wakax_ecommerce.produto.domain;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import javax.persistence.*;
@@ -23,10 +24,30 @@ public class Preco {
 
   @Column(nullable = false)
   @NotNull
-  @PositiveOrZero
+  @Positive
   private BigDecimal valor;
 
   @ManyToOne private Produto produto;
+
+  @Column(name = "data_de_cadastro")
+  private LocalDateTime dataDeCadastro;
+
+  @Column(name = "data_de_atualizacao")
+  private LocalDateTime dataDeAtualizacao;
+
+  @PrePersist
+  public void prePersist() {
+    this.dataDeCadastro = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    this.dataDeAtualizacao = LocalDateTime.now();
+  }
+
+  public void atualizaValor(BigDecimal novoValor) {
+    this.valor = novoValor;
+  }
 
   public Preco(TipoPreco tipo, BigDecimal valor, Produto produto) {
     this.tipo = tipo;
