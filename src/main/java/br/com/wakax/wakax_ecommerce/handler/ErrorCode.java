8a3203221fc4b3d067912acf8bd9dbfcbef0d105
Felipe Moraski <moraski060@gmail.com>
2026-05-11 01:15:40 -0300

    feat: Criação das exceptions e codigos dos erros

--- src/main/java/br/com/wakax/wakax_ecommerce/handler/ErrorCode.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/handler/ErrorCode.java
@@ -39,7 +39,8 @@ public enum ErrorCode {
   CLIENTE_JA_ATIVO("cliente.ja.ativo"),
   CLIENTE_JA_INATIVO("cliente.ja.inativo"),
   CLIENTE_INATIVO("cliente.inativo"),
-  TRANSICAO_STATUS_INVALIDA("transicao.status.invalida");
+  TRANSICAO_STATUS_INVALIDA("transicao.status.invalida"),
+  FORNECEDOR_INATIVO("fornecedor.inativo");
 
   private final String code;
 
