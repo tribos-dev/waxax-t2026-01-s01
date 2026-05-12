6aea0d7c02ed0e3db9cfbeed435481daaf626cf1
iago <iagopsilva073@gmail.com>
2026-05-12 10:04:12 -0300

    fix: remove duplicidade

--- src/main/java/br/com/wakax/wakax_ecommerce/handler/ErrorCode.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/handler/ErrorCode.java
@@ -48,7 +48,6 @@ public enum ErrorCode {
   PRODUTO_JA_ATIVO("produto.ja.ativo"),
   PRODUTO_JA_INATIVO("produto.ja.inativo"),
   CARRINHO_NAO_PERTENCE_AO_CLIENTE_AUTENTICADO("carrinho.nao.pertence.ao.cliente.autenticado"),
-  PRODUTO_JA_INATIVO("produto.ja.inativo"),
   RELATORIO_DATA_OBRIGATORIA("relatorio.data.obrigatoria"),
   RELATORIO_DATA_INVALIDA("relatorio.data.invalida");
 
